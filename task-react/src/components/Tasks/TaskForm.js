49a203587ab917aa1f3258b135b0d570e1a1eef4
maegaspa <maegaspa@student.42lyon.fr>
2024-01-17 18:09:32 +0100

    First commit, login, register, taskform pages, darkmode on every pages.

--- task-react/src/components/Tasks/TaskForm.js
+++ task-react/src/components/Tasks/TaskForm.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import { makeStyles, createTheme, ThemeProvider } from '@material-ui/core/styles';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
