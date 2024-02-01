5909cdd8fab6a8c035a8734aeecd7e2a3fd29b2a
maegaspa <maegaspa@student.42lyon.fr>
2024-02-01 20:47:37 +0100

    adding categoryService

---
+++ task-react/src/services/categoryService.js
@@ -0,0 +1,84 @@
+import api from '../utils/api';
+
+const categoryService = {
+	async getAllCategories(token) {
+		try {
+			const config = {
+				headers: {
+					'Content-Type': 'application/json',
+					'Accept' : 'application/json',
+					'Authorization': token
+				}
+			};
+
+			const response = await api.get('/api/categories/categories', config);
+			return response.data;
+		} catch (error) {
+			throw new Error('Error fetching categories: ' + error.message);
+		}
+	},
+
+	async createCategory(token, name, description, startDate = false, endDate = null, budget = 0) {
+		try {
+			const config = {
+				headers: {
+					'Content-Type': 'application/json',
+					'Accept' : 'application/json',
+					'Authorization': token
+				}
+			};
+
+			const response = await api.post('/api/categories/categories', { name, description, startDate, endDate, budget }, config);
+			return response.data;
+		} catch (error) {
+			throw new Error('Error creating category: ' + error.message);
+		}
+	},
+
+	async getCategoryById(token, categoryId) {
+		try {
+			const config = {
+				headers: {
+					'Authorization': token
+				}
+			};
+
+			const response = await api.get(`/api/categories/categories/${categoryId}`, config);
+			return response.data;
+		} catch (error) {
+			throw new Error('Error fetching category by ID: ' + error.message);
+		}
+	},
+
+	async updateCategory(token, categoryId, name, description, startDate = false, endDate = null, budget) {
+		try {
+			const config = {
+				headers: {
+					'Authorization': token
+				}
+			};
+
+			const response = await api.put(`/api/categories/categories/${categoryId}`, { name, description, startDate, endDate, budget }, config);
+			return response.data;
+		} catch (error) {
+			throw new Error('Error updating category: ' + error.message);
+		}
+	},
+
+	async deleteCategory(token, categoryId) {
+		try {
+			const config = {
+				headers: {
+					'Authorization': token
+				}
+			};
+
+			const response = await api.delete(`/api/categories/categories/${categoryId}`, config);
+			return response.data;
+		} catch (error) {
+			throw new Error('Error deleting category: ' + error.message);
+		}
+	},
+};
+
+export default categoryService;
